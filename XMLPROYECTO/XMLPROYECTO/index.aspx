93ecda5ded29ec884c68f271fdb3846d3d5b4de9
Fabrizio Polar <fmendozas@unsa.edu.pe>
2021-10-30 19:06:34 -0500

    Laboratorio 09

---
+++ XMLPROYECTO/XMLPROYECTO/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="XMLPROYECTO.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID ="cmdCreateXml" Text="Crear XML" runat="server" OnClick="cmdCreateXml_Click"/>
+            <asp:Button ID ="cmdReadXml" Text="Leer XML (Como texto)" runat="server"/>
+            <asp:Button ID ="cmdReadXmlAsObjects" Text="Leer XML (Como objeto)" runat="server"/>
+            <br />
+            <asp:Label ID="lblXml" runat="server"></asp:Label>
+            <asp:GridView ID ="gridResults" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ XMLPROYECTO/XMLPROYECTO/index.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace XMLPROYECTO
+{
+
+
+    public partial class index
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control cmdCreateXml.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdCreateXml;
+
+        /// <summary>
+        /// Control cmdReadXml.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdReadXml;
+
+        /// <summary>
+        /// Control cmdReadXmlAsObjects.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdReadXmlAsObjects;
+
+        /// <summary>
+        /// Control lblXml.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblXml;
+
+        /// <summary>
+        /// Control gridResults.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridResults;
+    }
+}
